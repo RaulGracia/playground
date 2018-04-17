8a83ce2aa0f56cb34aa6e5ea8b15535d3f8140a8
Raúl Gracia <raul.gracia@emc.com>
2018-04-17 10:44:05 +0200

    Added fix to test issue

--- src/main/java/MyCalculator.java
+++ src/main/java/MyCalculator.java
@@ -15,9 +15,22 @@ public class MyCalculator {
         return ((double)a)/b;
     }
 
-    public int multilply (int a, int b) {
+    public int multiply(int a, int b) {
         return a*b;
     }
 
+    public double sum (double a, double b) {
+        return a+b;
+    }
+
+    public double subtract (double a, double b) {
+        return a-b;
+    }
+
+    public double divide (double a, double b) { return a/b;}
+
+    public double multiply(double a, double b) {
+        return a*b;
+    }
 
 }
